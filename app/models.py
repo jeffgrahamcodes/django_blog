6a4f0d81e13595e9272337f437ddeed2984be0e9
J. Graham <polymathy78@gmail..com>
2025-02-25 19:22:39 -0500

    Create & register Django models

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+ARTICLE_STATUS = (
+    ("draft", "draft"),
+    ("in_progress", "in_progress"),
+    ("published", "published"),
+)
+
+
+class UserProfile(AbstractUser):
+    pass
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, default="")
+    word_count = models.IntegerField()
+    twitter_post = models.TextField(blank=True, default="")
+    status = models.CharField(
+        max_length=20,
+        choices=ARTICLE_STATUS,
+        default="draft"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
