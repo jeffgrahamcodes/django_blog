9babf140021b9b2018182cf5dc783e3abe1365ca
J. Graham <polymathy78@gmail..com>
2025-02-25 21:33:13 -0500

    create view foe handling forms

--- app/views.py
+++ app/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.models import Article
+from app.forms import CreateArticleForm
+
 
 def home(request):
     articles = Article.objects.all()
     return render(request, 'app/home.html', {'articles': articles})
 
+
+def create_article(request):
+    if request.method == 'POST':
+        form = CreateArticleForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            new_article = Article(
+                title=form_data['title'],
+                content=form_data['content'],
+                word_count=form_data['word_count'],
+                twitter_post=form_data['twitter_post'],
+                status=form_data['status']
+            )
+            new_article.save()
+
+            return redirect('home')
+    else:
+        form = CreateArticleForm()
+    return render(request, 'app/create_article.html', {'form': form})
