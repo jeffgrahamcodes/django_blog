d7da385d0735fd3a0e9bc84f75f73e29f27da661
J. Graham <polymathy78@gmail..com>
2025-02-28 15:58:06 -0500

    create article creator

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 import re
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
 
@@ -22,6 +23,9 @@ class Article(models.Model):
     status = models.CharField(max_length=20, choices=ARTICLE_STATUS, default="draft")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles"
+    )
 
     def save(self, *args, **kwargs):
         text = re.sub(r"<[^>]*>", "", self.content).replace("&nbsp;", " ")
