9df50e24844c2b1e6434f69c99c61cd8312ed31c
J. Graham <polymathy78@gmail..com>
2025-02-28 18:41:18 -0500

    implement login required mixin for article views and fix login header typo

--- app/views.py
+++ app/views.py
@@ -1,16 +1,23 @@
+from typing import Any
+
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from app.models import Article
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "app/index.html"
     context_object_name = "articles"
 
+    def get_queryset(self) -> QuerySet[Any]:
+        return Article.objects.filter(creator=self.request.user).order_by("-created_at")
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     fields = ["title", "status", "content", "twitter_post"]
     template_name = "app/article_create_form.html"
@@ -21,16 +28,22 @@ class ArticleCreateView(CreateView):
         return super().form_valid(form)
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ["title", "status", "content", "twitter_post"]
     template_name = "app/article_update_form.html"
     success_url = reverse_lazy("home")
     context_object_name = "article"
 
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "app/article_delete_form.html"
     success_url = reverse_lazy("home")
     context_object_name = "article"
+
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
